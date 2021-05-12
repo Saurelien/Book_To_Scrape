4f7faa284e2a622243fadadca0773836cc363cce
AScstema <git-commit-address>
2021-05-12 13:57:09 +0200

    modification du gitignore

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ GLOBAL_CAT_URL = 'https://books.toscrape.com/'
 CATEGORY_URL = 'https://books.toscrape.com/catalogue/category/books/fiction_10/index.html'
 
 def book_data(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
     booking = soup.find_all(class_='default')
 
     for books in booking:
@@ -20,7 +20,6 @@ def book_data(url):
         stock = availability.strip('In stock () available')
         description = books.find(class_='sub-header').find_next('p')# p7 description produit.
         book_description = description.text
-        print(book_description)
         category = books.find(class_='breadcrumb').find_next('a').text# p8 cathégory.
         review = books.find('p', class_='star-rating').get('class')[1] #"avis" je cherche a retourner la valeur de la position dans la class star-rating[0] Five[1]
         image_scrape = books.find('div', class_='item active').find_next('img')# p10 url de l'image.
@@ -39,27 +38,31 @@ def book_data(url):
             }
 
 def save_img(url, path):
-    for img in soup.find('div', class_='image_container').find_all('img', class_='thumbnail'):
-        img_url = img.get('src')
-        #print(img_url)
-        #with_name(path)
-            
-#save_img()
+    img_urls = []
+    soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+    for img in soup.find_all('div', class_='image_container').find_all('img', class_='thumbnail'):
+        img_url = img.attrs.get('src')
+    print(img_url)
+    if img_url == 'src':
+        img_url.append(next.find('a').get('href'))
+        with open(path, 'w')as f:
+            f.write(img_urls)
+    return img_urls
 
 def get_categories():
     global_cat_url = 'https://books.toscrape.com/'
     data = {}
     response = requests.get(global_cat_url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    get_categories_scrape = soup.find('div', class_='side_categories').find('li').find_all('li')
-    for category in get_categories_scrape:
+    category_scrape = soup.find('div', class_='side_categories').find('li').find_all('li')
+    for category in category_scrape:
         books_url = category.find('a', href = True).get('href')
         cat_title = category.text.strip() # afficher les données récupéré en utilisant une méthode qui récurpère uniquement le texte du lien "href" et de couper les espaces inutiles " .strip "
         data[cat_title] = global_cat_url + books_url  # data a pour clef la varaible cat_title des titres des categories et à comme valeur l'url des categories.
     return data
 
 # fonction qui boucle sur les livres d'une catégorie et les pages qui la compose
-def get_books_url(url):
+def get_books_data(url):
     links = []
     r = requests.get(url) # récupération de l'url pour le scrape
     soup = BeautifulSoup(r.content, 'html.parser') # méthode pour récupérer les données
@@ -69,10 +72,10 @@ def get_books_url(url):
         links.append(GLOBAL_CAT_URL + 'catalogue/' + books_url)
     next = soup.find('li', class_='next')
     if next is not None:
-        btn_next = url.split('/')[0 : -1] # destructuration de l'url category_url du dernier element "index.html"
-        btn_next.append(next.find('a').get('href')) # ajout de l'element ciblé sur la nouvel url "page-2.html"
-        btn_next1 = '/'.join(btn_next) # restructuration de l'url avec le nouvel element
-        links.extend(get_books_url(btn_next1))
+        next_page = url.split('/')[0 : -1] # destructuration de l'url category_url du dernier element "index.html"
+        next_page.append(next.find('a').get('href')) # ajout de l'element ciblé sur la nouvel url "page-2.html"
+        next_page_url = '/'.join(next_page) # restructuration de l'url avec le nouvel element
+        links.extend(get_books_data(next_page_url))
     return links          
 
 for category_name,category_url in get_categories().items():
@@ -80,6 +83,6 @@ for category_name,category_url in get_categories().items():
         fieldnames = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
         wr = csv.DictWriter(csvfile, fieldnames=fieldnames)
         wr.writeheader()
-        for url in get_books_url(category_url):
+        for url in get_books_data(category_url):
             book_info = book_data(url)
             wr.writerow(book_info)
